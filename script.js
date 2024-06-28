1f2fed146a84c196ac981e76d0f266900cc2d077
Warren <the.waz88@gmail.com>
2024-06-28 12:01:46 +0200

    Added all of the buttons to trigger correctly and added event listeners

--- script.js
+++ script.js
@@ -1,21 +1,42 @@
-const sumBtn = document.getElementById("sum");
-const subtractBtn = document.getElementById("subtract");
-const multiplyBtn = document.getElementById("multiply");
-const divideBtn = document.getElementById("divide");
-const equalsBtn = document.getElementById("result");
-const clearBtn = document.getElementById("clear");
+const sumBtn = document.getElementById("sum").addEventListener("click", () => operator = "+");
+const subtractBtn = document.getElementById("subtract").addEventListener("click", () => operator = "-");
+const multiplyBtn = document.getElementById("multiply").addEventListener("click", () => operator = "*");
+const divideBtn = document.getElementById("divide").addEventListener("click", () => operator = "/");
+const equalsBtn = document.getElementById("result").addEventListener("click", () => solution = equals());
+const clearBtn = document.getElementById("clear").addEventListener("click", () => solution = clear());
 const displaySolution = document.getElementById("solution");
 const displayFormula = document.getElementById("formula");
 
-let firstNum;
-let operator;
-let secondNum;
-let solution;
+const oneBtn = document.getElementById("one").addEventListener("click", () => operation("1"));
+const twoBtn = document.getElementById("two").addEventListener("click", () => operation("2"));
+const threeBtn = document.getElementById("three").addEventListener("click", () => operation("3"));
+const fourBtn = document.getElementById("four").addEventListener("click", () => operation("4"));
+const fiveBtn = document.getElementById("five").addEventListener("click", () => operation("5"));
+const sixBtn = document.getElementById("six").addEventListener("click", () => operation("6"));
+const sevenBtn = document.getElementById("seven").addEventListener("click", () => operation("7"));
+const eightBtn = document.getElementById("eight").addEventListener("click", () => operation("8"));
+const nineBtn = document.getElementById("nine").addEventListener("click", () => operation("9"));
+const zeroBtn = document.getElementById("zero").addEventListener("click", () => operation("0"));
+const dotBtn = document.getElementById("dot").addEventListener("click", () => operation("."));
 
-const sum = (a, b) => a + b;
-const subtract = (a, b) => a - b;
-const multiply = (a, b) => a * b;
-const divide = (a, b) => a / b;
+let firstNum = "";
+let operator = "";
+let secondNum = "";
+let solution = 0;
+
+const sum = (a, b) => Number(a) + Number(b);
+const subtract = (a, b) => Number(a) - Number(b);
+const multiply = (a, b) => Number(a) * Number(b);
+const divide = (a, b) => Number(a) / Number(b);
+
+const operation = (a) => {
+    if (!operator) {
+        firstNum += a
+    } else {
+        secondNum += a
+    }
+    displayFormula.innerText = firstNum + " " + operator + " " + secondNum
+}
 
 const equals = () => {
     if (operator && firstNum && secondNum) {
@@ -37,4 +58,13 @@ const equals = () => {
         }
         displaySolution.innerText = solution
     }
+}
+
+const clear = () => {
+    firstNum = "";
+    operator = "";
+    secondNum = "";
+    solution = "0";
+    displayFormula.innerText = "";
+    displaySolution.innerText = "0"
 }
