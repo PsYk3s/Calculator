42b3832953b422ff0fb337f6610f1806258c7942
Warren <the.waz88@gmail.com>
2024-06-28 10:18:52 +0200

    Added basic html layout, css and script to get started

---
+++ script.js
@@ -0,0 +1,12 @@
+const sumBtn = document.getElementById("sum");
+const subtractBtn = document.getElementById("subtract");
+const multiplyBtn = document.getElementById("multiply");
+const divideBtn = document.getElementById("divide");
+const equalsBtn = document.getElementById("result");
+const clearBtn = document.getElementById("clear");
+const displaySolution = document.getElementById("solution");
+const displayFormula = document.getElementById("formula");
+
+let firstNum;
+let operator;
+let secondNum;
