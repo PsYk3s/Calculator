3e9eb1348104cef73c15e3aec37c9ba82e35192c
Warren <the.waz88@gmail.com>
2024-06-28 11:16:50 +0200

    fixed style for viewing and added equals functionality

--- script.js
+++ script.js
@@ -10,3 +10,31 @@ const displayFormula = document.getElementById("formula");
 let firstNum;
 let operator;
 let secondNum;
+let solution;
+
+const sum = (a, b) => a + b;
+const subtract = (a, b) => a - b;
+const multiply = (a, b) => a * b;
+const divide = (a, b) => a / b;
+
+const equals = () => {
+    if (operator && firstNum && secondNum) {
+        switch (operator) {
+            case "+":
+                solution = sum(firstNum, secondNum)
+                break;
+            case "-":
+                solution = subtract(firstNum, secondNum)
+                break;
+            case "*":
+                solution = multiply(firstNum, secondNum)
+                break;
+            case "/":
+                solution = divide(firstNum, secondNum)
+                break;
+            default:
+                break;
+        }
+        displaySolution.innerText = solution
+    }
+}
